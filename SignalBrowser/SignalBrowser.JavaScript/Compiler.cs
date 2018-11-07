9d38bbc0992c0164bbf253257ce264710554f2e2
Ali Yousefi <ali.visual.studio@gmail.com>
2018-11-07 11:42:47 +0330

    start with javascript language

---
+++ SignalBrowser/SignalBrowser.JavaScript/Compiler.cs
@@ -0,0 +1,13 @@
+﻿using SignalBrowser.ProgrammingLanguage;
+using System;
+
+namespace SignalBrowser.JavaScript
+{
+    public class Compiler : ICompiler
+    {
+        public IBlockInfo ICompiler(string sourceCode)
+        {
+
+        }
+    }
+}
